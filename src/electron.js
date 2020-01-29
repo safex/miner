acfd2eed147cf09ef7842015fa01eae33d7f741a
Daniel Dabek <xordaniel@gmail.com>
2020-01-28 21:09:28 -0500

    remove dev tools

--- src/electron.js
+++ src/electron.js
@@ -43,7 +43,7 @@ function createWindow() {
 
     mainWindow.loadURL(startUrl);
 
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
